acd121fce441f8416f75f41680eb4bb019262167
MarisolOrtiz <64797672+MarisolOrtiz@users.noreply.github.com>
2020-11-04 14:09:38 -0600

    elementos corregidos

--- app/src/main/java/com/example/volley/Logoon.java
+++ app/src/main/java/com/example/volley/Logoon.java
@@ -50,41 +50,88 @@ public class Logoon extends AppCompatActivity {
         ini = findViewById(R.id.btninicio);
         ET_USU = findViewById(R.id.ET_USU);
         ET_CLA = findViewById(R.id.ET_CLA);
-         ini.setOnClickListener(new View.OnClickListener() {
+
+
+        ini.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validarUsuario("http://192.168.43.243/service2020/validarUsuario.php");
+                Thread tr = new Thread(){
+                    @Override
+                    public void run() {
+                        final String resultado = Ingresar(ET_USU.getText().toString(), ET_CLA.getText().toString());
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                int r = obtenerDatosJson(resultado);
+                                if (r > 0){
+                                    Intent i = new Intent(Logoon.this , MainActivity.class);
+                                    i.putExtra("usuario", ET_USU.getText().toString());
+                                    startActivity(i);
+                                    ET_USU.setText(null);
+                                    ET_CLA.setText(null);
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Usuario incorrecto", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    }
+                };
+                tr.start();
             }
         });
     }
-    private void validarUsuario(String URL){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (!response.isEmpty()){
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(Logoon.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+
+    public String Ingresar(String usuario, String Clave){
+
+        URL url = null;
+        String linea = "";
+        int respuesta=0;
+        StringBuilder resul=null;
+
+        try {
+
+            url = new URL("http://192.168.43.243/service2020/validarUsuario.php?usuario="+usuario+"&clave="+Clave);
+
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            respuesta = connection.getResponseCode();
+
+            resul = new StringBuilder();
+
+            if (respuesta==HttpURLConnection.HTTP_OK){
+
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                while ((linea = reader.readLine()) !=null){
+                    resul.append(linea);
                 }
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(Logoon.this, "Error de conexion ", Toast.LENGTH_LONG).show();
-            }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> parametros = new HashMap<String, String>();
-                parametros.put("correo",ET_USU.getText().toString());
-                parametros.put("clave", ET_CLA.getText().toString());
-                return parametros;
-            }
-        };
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        }catch (Exception e){
+
+        }
+
+        return resul.toString();
+    }
+
+    public int obtenerDatosJson(String response){
+        int res = 0;
+
+        try {
+
+            JSONArray json = new JSONArray(response);
+
+            if (json.length() > 0 ){
+                res = 1;
+            }
+
+        }catch (Exception e){
+
+        }
+
+        return res;
     }
 
 }
